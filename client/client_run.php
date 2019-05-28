054f9ae7bf9a95a69a9366ba03b43ec2db756df9
Артем Артемов <artemoff89@gmail.com>
2019-05-28 23:54:58 +0300

    Add some modification

--- client/client_run.php
+++ client/client_run.php
@@ -6,5 +6,6 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=benchmark password=postgre
 for ($i = 0; $i < 10; $i++) {
   pg_query_params($dbconn, 'SELECT * FROM pgbench_accounts WHERE aid = $1', array(rand(0,15000000)));
   pg_query_params($dbconn, "SELECT * FROM \r\npgbench_accounts WHERE \r\naid = $1", array(rand(0,15000000)));
+  pg_query($dbconn, "SELECT * FROM \r\npgbench_accounts WHERE \r\naid = 1245000");
 }
 ?>
