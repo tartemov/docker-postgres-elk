211b50d26aec07713c54eedebc7fd775c6f68c4b
Артем Артемов <artemoff89@gmail.com>
2019-05-28 17:20:13 +0300

    Add filebeat configuration for real PostgreSQL logs

--- client/client_run.php
+++ client/client_run.php
@@ -4,7 +4,7 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=benchmark password=postgre
 
 // Run some queries
 for ($i = 0; $i < 10; $i++) {
-  pg_query_params($dbconn, 'SELECT * FROM pg_sleep($1)', array(rand(0,2)));
   pg_query_params($dbconn, 'SELECT * FROM pgbench_accounts WHERE aid = $1', array(rand(0,15000000)));
+  pg_query_params($dbconn, "SELECT * FROM \r\npgbench_accounts WHERE \r\naid = $1", array(rand(0,15000000)));
 }
 ?>
